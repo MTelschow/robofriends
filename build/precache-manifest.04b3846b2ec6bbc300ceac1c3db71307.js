c762fb502161c60bc551a6fed54d82e68ed4cda8
Mathis Telschow <mathistelschow@gmail.com>
2023-03-24 18:06:04 +0100

    Add changes

---
+++ build/precache-manifest.04b3846b2ec6bbc300ceac1c3db71307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "918e542068d7a0a6b471",
+    "url": "/static/css/main.99040eee.chunk.css"
+  },
+  {
+    "revision": "918e542068d7a0a6b471",
+    "url": "/static/js/main.46b2a68b.chunk.js"
+  },
+  {
+    "revision": "345d2fe2806c5c9be170",
+    "url": "/static/js/runtime~main.680bbd6c.js"
+  },
+  {
+    "revision": "808cb915db6263f2f9a6",
+    "url": "/static/css/2.a11f27dd.chunk.css"
+  },
+  {
+    "revision": "808cb915db6263f2f9a6",
+    "url": "/static/js/2.932c92d1.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "3b9c973625282dfc77e1a275c0ebf343",
+    "url": "/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+
+ReactDOM.render(
+  <div>
     <App />
-  </React.StrictMode>
+  </div>
+  , 
+  document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <h1>Robofriends</h1>
+//   <Hello />
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
